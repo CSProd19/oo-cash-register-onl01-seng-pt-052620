317b2a13cf95a1c233f19cbd336cf8a6d11e7ad5
Maria Connie Sanchez <msnchz04@gmail.com>
2020-06-22 18:19:18 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,23 @@
-
+class CashRegister
+  attr_accessor :discount, :total 
+  
+  def initialize(discount = 20)
+    @total = 0
+    @discount = discount
+    @items = []
+  end 
+  
+  def add_item(title, price, quantity = 1)
+     self.total += price * quantity
+  end 
+  
+  def apply_discount
+    if discount != 0
+      self.total = (total * ((100.0 - discount.to_f)/100)).to_i
+        "After the discount, the total comes to $#{self.total}."
+    else 
+        "There is no discount to apply."
+    end 
+  end
+  
+end 
